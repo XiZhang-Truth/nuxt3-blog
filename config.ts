0f70c54e828df954d1344e18edcd138a97557a6a
XiZhang-Truth <117990019+XiZhang-Truth@users.noreply.github.com>
2022-11-11 20:42:53 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,21 +1,21 @@
 export default {
-  title: "yun yu yuan", // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: "yunyuyuan", // 必须修改，github账户名
+  title: "西漳真心话", // 网站标题
+  nickName: "西漳真心话", // 昵称
+  githubName: "XiZhang-Truth", // 必须修改，github账户名
   githubRepo: "nuxt3-blog", // 勿修改
   githubBranch: "master", // 勿修改
-  domain: "https://blog.yunyuyuan.net", // rss域名
-  SEO_title: " - yunyuyuan blog", // 搜索引擎显示的标题
-  SEO_description: "云与原的博客，书写值得书写的东西", // description meta header
-  SEO_keywords: "yunyuyuan,yunyuyuan's blog,云与原,云与原的博客", // keywords meta header
+  domain: "https://www.xzzxh.tk", // rss域名
+  SEO_title: " - 西漳真心话", // 搜索引擎显示的标题
+  SEO_description: "XiZhang-Truth", // description meta header
+  SEO_keywords: "XiZhang-Truth", // keywords meta header
 
   themeColor: "#2aa0bb", // 主题色
   CommentRepoId: "R_kgDOGRGGfg", // https://giscus.app/zh-CN
   CommentDiscussionCategoryId: "DIC_kwDOGRGGfs4CQ5Jp", // https://giscus.app/zh-CN
   Comment: {
     articles: true,
-    records: false,
-    knowledges: false
+    records: true,
+    knowledges: true
   }, // 是否开启评论
   CloudflareAnalyze: "89460884e1e44e02a7320077f16d620f" // cloudflare的webanalyze
 };
